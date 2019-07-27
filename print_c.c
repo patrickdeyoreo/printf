3a214fecda253df6f5cb506ea6fa187ef4e3facd
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-26 18:53:07 -0700

    Added function documentation for print_c

--- print_c.c
+++ print_c.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * print_c - print a character
+ * @args: the va_list with the character to print as it's next element
+ *
+ * Return: the number of bytes printed
+ */
 int print_c(va_list args)
 {
 	return (_putchar(va_arg(args, int)));
