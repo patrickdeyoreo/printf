abc1673ece1a81684fbcfc592aab353ca982d61b
rajsudeep <sudrocks@gmail.com>
2019-07-27 01:08:06 +0000

    Add print prototypes and struct

--- holberton.h
+++ holberton.h
@@ -3,4 +3,21 @@
 
 int _putchar(int c);
 
+/**
+ * struct print_func - points to print functions relative to specifier
+ *
+ * @specifier: specifier format types
+ * @f: functions that print specific format types
+ */
+typedef struct print_func
+{
+	char *specifier;
+	int (*f)(va_list);
+} t_print_func;
+int (*get_print_func(char *c))(va_list);
+int print_c(va_list);
+int print_s(va_list);
+int print_prcnt(va_list);
+int _printf(const char *format);
+
 #endif /* HOLBERTON_H */
