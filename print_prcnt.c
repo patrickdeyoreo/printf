a6ec95c33cec5ae3c5e0737d11710d7bbb922ac4
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-26 19:24:29 -0700

    Adding print_prcnt.c with print_prcnt function def

---
+++ print_prcnt.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * print_prcnt - print a percent character
+ * @args: the va_list
+ *
+ * Return: the number of bytes printed
+ */
+int print_prcnt(va_list args)
+{
+	return (_putchar('%'));
+}
