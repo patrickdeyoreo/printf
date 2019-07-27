6a81a347ab85a587829fd11c9c26d13e53358d0c
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-26 19:17:40 -0700

    Updated holberton.h with prototypes for print_i & _print_i

--- holberton.h
+++ holberton.h
@@ -18,6 +18,8 @@ typedef struct print_func
 int (*get_print_func(char *c))(va_list);
 int print_c(va_list);
 int print_s(va_list);
+int print_i(va_list);
+void _print_i(int n, int *count);
 int print_prcnt(va_list);
 int _printf(const char *format);
 
