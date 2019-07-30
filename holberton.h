ddf2005e823f3799763b6a6a128cd66d5b53ec8e
patrickdeyoreo <pdeyoreo@gmail.com>
2019-07-30 07:16:56 -0700

    Implemented p spec, updated header, & added to get_print_func

--- holberton.h
+++ holberton.h
@@ -5,9 +5,9 @@
 #include <stdio.h>
 
 /**
- * struct print_func - points to print functions relative to specifier
- * @specifier: specifier format types
- * @f: functions that print specific format types
+ * struct print_func - map a print function to a conversion specifier
+ * @specifier: the conversion specifier
+ * @f: the function to call to format and print output
  */
 typedef struct print_func
 {
@@ -16,26 +16,41 @@ typedef struct print_func
 } t_print_func;
 
 int _printf(const char *format, ...);
-int _putchar(int c);
+
 int (*get_print_func(char c))(va_list);
+
 int print_c(va_list);
+
 int print_s(va_list);
+
+int print_prcnt(va_list);
+
 int print_i(va_list);
 void _print_i(int n, int *count);
+
 int print_b(va_list);
 void _print_b(unsigned int n, int *count);
+
 int print_u(va_list);
 void _print_u(unsigned int n, int *count);
+
 int print_o(va_list);
 void _print_o(unsigned int n, int *count);
+
 int print_x(va_list);
 void _print_x(unsigned int n, int *count);
+
 int print_X(va_list);
 void _print_X(unsigned int n, int *count);
-int print_prcnt(va_list);
-void _print_rev(char *s, int *count);
+
+int print_p(va_list);
+void _print_p(unsigned long int n, int *count);
+
 int print_rev(va_list);
+void _print_rev(char *s, int *count);
+
 int print_rot13(va_list);
-int _printf(const char *format, ...);
+
+int _putchar(int c);
 
 #endif /* HOLBERTON_H */
